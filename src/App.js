9af1fb6d00cd35eeaf82e45d9ef940533b06c16a
Anna <anna.svrva@gmail.com>
2023-08-22 18:31:07 +0300

    Created main elements: Header, Main, Footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import Header from "./Header";
+import Content from "./Content";
+import Footer from "./Footer";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Grocery List</header>
+      <Header />
+      <Content />
+      <Footer />
     </div>
   );
 }
